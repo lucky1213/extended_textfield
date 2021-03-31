1ee414bb9be4624ca45600bcc2b7eb1658c844a8
lucky1213 <lucky1213@vip.qq.com>
2021-03-31 16:19:15 +0800

    init commit

---
+++ lib/focus_node.dart
@@ -0,0 +1,52 @@
+part of extended_textfield;
+
+class SelectableFocusNode extends FocusNode {
+  SelectableFocusNode([this.allowFocus = true]) : super();
+  final bool allowFocus;
+}
+
+class ExtendedFocusNode extends FocusNode {
+  ExtendedFocusNode([this.keyboardToken = true]) : super();
+
+  bool keyboardToken;
+
+  // @override
+  // bool get hasFocus => _hasFocus;
+
+  @override
+  bool consumeKeyboardToken() {
+    return keyboardToken;
+  }
+
+  void hideKeyboard() {
+    SystemChannels.textInput.invokeMethod<void>('TextInput.hide');
+  }
+
+  void showKeyboard() {
+    SystemChannels.textInput.invokeMethod<void>('TextInput.show');
+  }
+
+  @override
+  void unfocus({
+    UnfocusDisposition disposition = UnfocusDisposition.scope,
+    bool showCursor = false,
+  }) {
+    keyboardToken = false;
+    if (showCursor) {
+      hideKeyboard();
+    } else {
+      super.unfocus(disposition: disposition);
+    }
+  }
+
+  @override
+  void requestFocus([FocusNode node, bool keyboard = true]) {
+    keyboardToken = keyboard;
+    if (keyboardToken) {
+      showKeyboard();
+    } else {
+      hideKeyboard();
+    }
+    super.requestFocus();
+  }
+}
